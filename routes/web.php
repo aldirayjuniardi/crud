df4d7a4faec5ab6a996b6025268886da564bb7d2
aldirayjuniardi <150163085+aldirayjuniardi@users.noreply.github.com>
2023-11-23 16:17:30 +0700

    submision2

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+//127.0.0.1:8000/ ==> view welcome
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//127.0.0.1:8000/ ==> view welcome
+Route::get('/mahasiswa', function () {
+    return view('mahasiswa.index');
+});
